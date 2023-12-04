4e6941fc5120e11fe4f14018a485b3d54b5611c4
Suhana Ayisha <31471461+suhanaayisha@users.noreply.github.com>
2023-12-04 16:01:58 +0400

    fix inputs

--- main.py
+++ main.py
@@ -42,64 +42,67 @@ print("no_of_features = {}".format(no_of_features))
 
 
 
-# hidden_layers = int(input("Enter number of hiddhen layers (excluding input and output layer): "))
+hidden_layers = int(input("Enter number of hiddhen layers (excluding input and output layer): "))
 
 
 
 
-# no_of_layers = 2 + hidden_layers
-# print("Total no. of layers", no_of_layers)
+no_of_layers = 2 + hidden_layers
+print("Total no. of layers", no_of_layers)
+
+print("Enter No. of nodes per hidden layer: ")
+nodes_per_hidden_layer=[]
+for i in range(0, hidden_layers):
+    ele = int(input(f"Layer {i+1}: "))
+    nodes_per_hidden_layer.append(ele)  
+ 
+
+
+print("No. of nodes per hidden layer", nodes_per_hidden_layer)
+nodes_per_layer = [no_of_features]+nodes_per_hidden_layer+[1]
 
 
 
 
 
-# nodes_per_hidden_layer = list(map(int, 
-#     input("\nEnter the number of nodes in each hidden layers (separated by space).\n For eg. 3 2 1, if you have 3 hidden layers : ").strip().split()))[:hidden_layers]
-
-# print("No. of nodes per hidden layer", nodes_per_hidden_layer)
-
-# nodes_per_layer = [no_of_features]+nodes_per_hidden_layer+[1]
-
-
-
-
-
-# print("No. of nodes per layer", nodes_per_layer)
+print("No. of nodes per layer:", nodes_per_layer)
 
 sigmoid = Sigmoid()
 relu = Relu()
 tanh = Tanh()
 
-# functions = [relu,sigmoid,relu,tanh]
+print("Enter activation functions per hidden layer: ")
+functions_per_layer_str=[]
+for i in range(0, hidden_layers):
+    func_str = input(f"Layer {i+1}: ")
+    functions_per_layer_str.append(func_str)  
+ 
+
+functions_per_layer_str = ['s']+functions_per_layer_str+['s']
+print("Activation functions per layer:", functions_per_layer_str)
 
 
-# functions_per_layer_str = list(map( str,
-#     input("\nEnter the activation functions for each layer (including input and output layer) (separated by space).\n Enter 's' for sigmoid, 'r' for relu and 't' for tan. \n For eg. if there are total of 5 layers s r s t s : ").strip().split()))
+def map_to_activation(s):
+    func= sigmoid
 
+    if func == 's': 
+        func= sigmoid
 
-# def map_to_activation(s):
-#     func= sigmoid
+    elif func == 'r': 
+        func= relu
 
-#     if func == 's': 
-#         func= sigmoid
-
-#     elif func == 'r': 
-#         func= relu
-
-#     elif func == 't': 
-#         func= tanh
+    elif func == 't': 
+        func= tanh
         
-#     return func
+    return func
 
 
-# functions_per_layer = map(map_to_activation,functions_per_layer_str)
-# functions_per_layer= list(functions_per_layer);
-# print("functions_per_layer\n",len(functions_per_layer))
+functions_per_layer = map(map_to_activation,functions_per_layer_str)
+functions_per_layer= list(functions_per_layer);
+print("functions_per_layer\n",functions_per_layer)
 
+print("Building Ann and PSO...")
 loss_func = Binary_cross_entropy()
-
-# ann = ANNBuilder.build(no_of_layers, nodes_per_layer, functions_per_layer)
 
 
 #Testing: 
@@ -107,7 +110,8 @@ loss_func = Binary_cross_entropy()
 # here [no_of_features, 3, 8 ,4, 3] -> this array, you can change the rest of the numbers after no_of_features, accordinf to no. of layers. So if your number of layers
 # is 4 then the araay would have no_of_features, followed by 3 more numbers
 
-ann = ANNBuilder.build(5, [no_of_features, 3, 8 ,4, 3], [sigmoid, sigmoid, sigmoid, sigmoid, sigmoid])
+ann = ANNBuilder.build(no_of_layers, nodes_per_layer, functions_per_layer)
+# ann = ANNBuilder.build(5, [no_of_features, 3, 8 ,4, 3], [sigmoid, sigmoid, sigmoid, sigmoid, sigmoid])
 
 
 annFunc = AdapterFunction(ann, X_train, y_train, loss_func)
